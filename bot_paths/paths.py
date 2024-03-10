cde870617f394392adf15403cfe1a852c318ac88
AndreiOsipov <osipov.ekaterinburg@gmail.com>
2024-03-10 18:16:09 +0500

    service accaount

--- bot_paths/paths.py
+++ bot_paths/paths.py
@@ -4,5 +4,4 @@ import os
 ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 TOKENS_PATH = os.path.join(ROOT, "tokens")
 BOT_TOKEN = os.path.join(TOKENS_PATH, "bot_token.json")
-GOOGLE_API_TOKEN_PATH = os.path.join(TOKENS_PATH, "token.json")
-GOOGLE_API_CREDS_PATH = os.path.join(TOKENS_PATH, "credentials.json")
+GOOGLE_API_CREDS_PATH = os.path.join(TOKENS_PATH, "service_key.json")
--- spreadsheets/spreadsheet.py
+++ spreadsheets/spreadsheet.py
@@ -1,12 +1,9 @@
 import os
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
-from googleapiclient.http import MediaIoBaseDownload
 
 
-from bot_paths.paths import GOOGLE_API_TOKEN_PATH, GOOGLE_API_CREDS_PATH
+from bot_paths.paths import GOOGLE_API_CREDS_PATH
 
 SCOPES = [
     'https://www.googleapis.com/auth/spreadsheets',
@@ -18,24 +15,7 @@ LIST_ID = 0
 
 class GoogleAPILogin:
     def get_auth_creds(self):
-        """возвращает разрешения на использование гугл-таблиц и гугл-диска"""
-        creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-        if os.path.exists('token.json'):
-            creds = Credentials.from_authorized_user_file(GOOGLE_API_TOKEN_PATH, SCOPES)
-        # If there are no (valid) credentials available, let the user log in.
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(GOOGLE_API_CREDS_PATH, SCOPES)
-                creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            with open(GOOGLE_API_TOKEN_PATH, 'w') as token:
-                token.write(creds.to_json())
-        return creds
+        return Credentials.from_service_account_file(GOOGLE_API_CREDS_PATH)
 
 class QueriesGoogleSheets:
     def __init__(self) -> None:
